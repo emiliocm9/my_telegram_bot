72d27268aff2630b839e5be1903cb9817367d9fc
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-12 00:26:17 -0500

    gem install

---
+++ lib/bot.rb
@@ -0,0 +1,11 @@
+require 'telegram/bot'
+require 'geocoder'
+require_relative 'covid_api'
+
+class Bot
+  def initialize
+    token = '1175332363:AAFsytcuXCfYSGpWPpcYRlWJ7uOPEvbWxZE'
+  
+  end
+
+end
