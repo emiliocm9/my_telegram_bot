4d31896d4b473dc8132b20ba02cce17e1101200d
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-14 12:41:50 -0500

    Create get information method

--- lib/header_news.rb
+++ lib/header_news.rb
@@ -1,7 +1,7 @@
-require 'news-api'
+require 'net/http'
 require 'uri'
 
-uri = URI.parse("https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=991a6bc8a2bdcd65a5cbeb076b133b05")
+uri = URI.parse("https://financialmodelingprep.com/api/v3/profile/MSFT?apikey=991a6bc8a2bdcd65a5cbeb076b133b05")
 request = Net::HTTP::Get.new(uri)
 request["Upgrade-Insecure-Requests"] = "1"
 
@@ -13,23 +13,5 @@ response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
   http.request(request)
 end
 
-# response.code
-# response.body
-
-class NewsApi
-
-  # attr_reader :top_headlines
-
-  def initialize
-    newsapi = News.new('bb3ba6f0ed7f48689708122da76ecdcc')
-  end
-
-  def top_headlines
-    newsapi.get_top_headlines(q: 'bitcoin',
-    sources: 'bbc-news,the-verge',
-    category: 'business',
-    language: 'en',
-    country: 'us')
-  end
-  
-end
+response.code
+p response.body
--- lib/motive.rb
+++ lib/motive.rb
@@ -2,77 +2,29 @@ require 'telegram/bot'
 require 'net/http'
 require 'json'
 require 'uri'
-require 'net/http'
-require 'json'
 
 class CovidApi
-  attr_reader :commands
+  attr_reader :uri
   def initialize
-    @url = 'https://api.covid19api.com/'
+    @uri = URI.parse("https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=991a6bc8a2bdcd65a5cbeb076b133b05")
   end
 
-  def summary
-    organize_output(get_information('summary') { |hash| hash['Global'] })
-  end
+  def get_information
+    request = Net::HTTP::Get.new(uri)
+    request["Upgrade-Insecure-Requests"] = "1"
 
-  def countries
-    country_array = get_information('summary') { |hash| hash['Countries'] }
-    country_array.map! { |el| el['Slug'] }
-    country_array.sort
-  end
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
 
-  def country(country)
-    country_hash = get_information('total/country/' + country) { |arr| arr[arr.length - 1] } if country.ascii_only?
-
-    if country == 'Error'
-      'There is no data for this location available, try using the slug name.'
-    elsif country_hash
-
-      selected_country_fields = country_hash.select { |k, _| %w[Confirmed Country Deaths Recovered Active].include?(k) }
-
-      text_output = "Country: #{selected_country_fields['Country']}\n"
-
-      text_output += organize_output(selected_country_fields.reject { |k, _| k == 'Country' })
-      text_output
-    else
-      organize_output(country.split('-').join(' ').capitalize + ' has no data on Api')
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
     end
-  end
 
-  def get_slug_country(input_country)
-    country_array = get_information('summary') { |hash| hash['Countries'] }
-    country_array_with_slug = country_array.map { |h| h.select { |k, _| %w[Slug Country].include?(k) } }
-    index = country_array_with_slug.find_index { |el| el['Country'].downcase == input_country.downcase }
-    if index
-      country_array[index]['Slug']
-    else
-      'Error'
-    end
-  end
-
-  private
-
-  def get_information(path)
-    url_path = URI(@url + path)
-    https = Net::HTTP.new(url_path.host, url_path.port)
-    https.use_ssl = true
-
-    request = Net::HTTP::Get.new(url_path)
-    response = https.request(request)
-    summary_hash = JSON.parse(response.read_body)
-
-    yield(summary_hash)
-  end
-
-  def organize_output(object)
-    return object if object.is_a? String
-
-    text_output = ''
-    object.each do |key, value|
-      value = value.to_s.reverse.scan(/\d{1,3}/).join(',').reverse
-      key = key.split(/(?=[A-Z])/).join(' ')
-      text_output += "#{key}: #{value}\n"
-    end
-    text_output
+    # response.code
+    response.body
   end
 end
+
+AAPL = CovidApi.new
+p AAPL.get_information
