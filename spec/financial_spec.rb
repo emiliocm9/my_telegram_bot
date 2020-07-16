b9ca405a8c27f792a40cddb0f1793dd71bcfc5b8
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-15 19:39:53 -0500

    FIX stickler errors

--- spec/financial_spec.rb
+++ spec/financial_spec.rb
@@ -7,11 +7,11 @@ describe FinStatus do
     let(:symbol_wrong) { finance.get_finance('ALSEA') }
     let(:symbol_right) { finance.get_finance('NFLX') }
     it 'Will display an error message if the STOCK symbol does not exist.' do
-      expect(p symbol_wrong).to eql('Error: please type the STOCK symbol again.')
+      expect((p symbol_wrong)).to eql('Error: please type the STOCK symbol again.')
     end
 
     it 'Returns a string including a financial statements' do
-      expect((p symbol_right).include?('financials')).to eql(true)
+      expect(p symbol_right.include?('financials')).to eql(true)
     end
   end
 end
